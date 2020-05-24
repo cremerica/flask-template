fa4051e235f13efc1193d63d66bcd3e3c553bf4c
cremerfc <cremerfc@users.noreply.github.com>
2020-05-24 12:48:22 -0400

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,20 @@
+  
+from flask import Flask, render_template, request
+import requests
+import json
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    
+    return "You have reached Flask-app! Please make yourself at home!"
+
+@app.route('/test')
+def test():
+    return "This is the test"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
