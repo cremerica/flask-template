68298a6a7f856267801a5c6681330d0a2bb49857
cremerfc <cremerfc@users.noreply.github.com>
2020-05-26 14:40:44 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     
-    return "You have reached Flask-app! Please make yourself at home!"
+    return render_template("index.html")
 
 @app.route('/test')
 def test():
