2c4637f65f6282d113934461b8f20d82ce627fb1
cremerfc <cremerfc@users.noreply.github.com>
2020-06-03 19:51:48 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -8,15 +8,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
+#this method renders an html file located in the AppDirect/app/templates directory.
 def index():
     
     return render_template("index.html")
 
 @app.route('/test')
+# this was simply a test to see how to display a message by using a different route.
 def test():
     return "This is the test"
 
 @app.route('/support')
+# This creates a file and dumps all environment variables. Used as an example of creating a file for support to troubleshoot.
 def create_support_file():
     file_path = 'app/'
     file_name = 'env_vars.txt'
@@ -27,7 +30,9 @@ def create_support_file():
     file_to_write.write(file_content)
     file_to_write.close()
 
+
 @app.route('/msg')
+# This displays a message that can be passed as an evironment variable PAGE_MESSAGE
 def display_message():
     message_to_display = os.environ["PAGE_MESSAGE"]
     return message_to_display
