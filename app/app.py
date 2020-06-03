b055e9846a67daa3710e8a289ccf1d209979f999
cremerfc <cremerfc@users.noreply.github.com>
2020-06-03 11:40:04 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 import json
+import os
 
 
 app = Flask(__name__)
@@ -14,6 +15,11 @@ def index():
 @app.route('/test')
 def test():
     return "This is the test"
+
+@app.route('/msg')
+def display_message():
+    message_to_display = os.environ["PAGE_DISPLAY"]
+    return message_to_display
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
